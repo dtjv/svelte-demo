2853ea9f62c11fc8304799ea61b4b029fefd3670
David Valles <davidtjvalles@gmail.com>
2021-03-28 15:23:41 -0700

    Add edit button focus action

--- src/components/todo-item.svelte
+++ src/components/todo-item.svelte
@@ -9,6 +9,7 @@
   // local
   const dispatch = createEventDispatcher()
   let isEditing = false
+  let isEditBtnPressed = false
   let task = todo.task
   let taskEl: HTMLElement
 
@@ -36,14 +37,32 @@
 
   // after setting `isEditing`, svelte needs to re-render the dom, and mount
   // the taskEl node. so, we wait a tick before accessing `taskEl`.
+  //
+  // a better way to focus the input task field is:
+  // 1. remove: `let taskEl`, `await tick()` and `taskEl.focus()`
+  // 1. remove `async` from `onEdit`
+  // 1. create a new action:
+  //      const focusOnInit = (node) =>
+  //        typeof node?.focus === 'function' && node.focus()
+  // 1. use action;
+  //      <input use:focusOnInit ...etc />
   async function onEdit() {
     isEditing = true
+    isEditBtnPressed = true
     await tick()
     taskEl.focus()
   }
 
   function onToggle() {
     updateTodo({ completed: !todo.completed })
+  }
+
+  // action
+  function focusEditButton(node: HTMLButtonElement): void {
+    if (typeof node?.focus !== 'function') {
+      throw new Error('Use action for HTMLButtonElement only')
+    }
+    isEditBtnPressed && node.focus()
   }
 </script>
 
@@ -79,7 +98,7 @@
       <input type="checkbox" checked={todo.completed} on:click={onToggle} />
       {todo.task}
     </label>
-    <button type="button" on:click={onEdit}>
+    <button type="button" on:click={onEdit} use:focusEditButton>
       Edit <span class="sr-only">{todo.task}</span>
     </button>
     <button type="button" on:click={onRemove}>
